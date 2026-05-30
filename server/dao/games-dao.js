8c9c232de142c134e294ba794ba147fea69eba61
Jenny Molander <jennymol@stud.ntnu.no>
2026-05-30 13:31:22 +0200

    Implement games-dao

---
+++ server/dao/games-dao.js
@@ -0,0 +1,154 @@
+import db from "../db.js";
+import { Game, GameStep, User } from "../entities.js";
+
+const mapRowToGame = (row) => {
+    return new Game(
+        row.id,
+        row.userId,
+        row.startStationId,
+        row.destinationStationId,
+        row.status,
+        row.score,
+        row.createdAt
+    );
+}
+
+// Create new game
+export const createGame = (userId, startStationId, destinationStationId) => {
+    return new Promise((resolve, reject) => {
+        const sql = `INSERT INTO games (
+            userId,
+            startStationId,
+            destinationStationId,
+            status,
+            score)
+            VALUES (?, ?, ?, ?, ?)
+        `;
+
+        // createdAt with game start time is handled automatically by DEFAULT CURRENT_TIMESTAMP
+        db.run(sql, [userId, startStationId, destinationStationId, 'planning', 20], function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(this.lastID);
+            }
+        });
+    });
+}
+
+// Get game by Id
+export const getGame = (gameId) => {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM games WHERE id = ?";
+
+        db.get(sql, [gameId], (err, row) => {
+            if (err) {
+                reject(err);
+            } else if (row === undefined) {
+                resolve(false);
+            } else {
+                const game = mapRowToGame(row);
+
+                resolve(game);
+            }
+        });
+    });
+}
+
+// Update game score
+export const updateGameScore = (gameId, score) => {
+    return new Promise((resolve, reject) => {
+        const sql = `UPDATE games
+            SET score = ?
+            WHERE id = ?
+        `;
+
+        db.run(sql, [score, gameId], function (err) {
+            if (err) {
+                reject(err);
+            } else if (this.changes !== 1) {
+                resolve(false);
+            } else {
+                resolve(true);
+            }
+        });
+    });
+};
+
+// Update game status
+export const updateGameStatus = (gameId, status) => {
+    return new Promise((resolve, reject) => {
+        const sql = `UPDATE games
+            SET status = ?
+            WHERE id = ?
+        `;
+
+        db.run(sql, [status, gameId], function (err) {
+            if (err) {
+                reject(err);
+            } else if (this.changes !== 1) {
+                resolve(false);
+            } else {
+                resolve(true);
+            }
+        });
+    });
+};
+
+// Save one execution step
+export const saveGameStep = (gameId, stepOrder, fromStationId, toStationId, eventId, coinsAfterStep) => {
+    return new Promise((resolve, reject) => {
+        const sql = `INSERT INTO game_steps (
+            gameId,
+            stepOrder,
+            fromStationId,
+            toStationId,
+            eventId,
+            coinsAfterStep)
+            VALUES (?, ?, ?, ?, ?, ?)
+        `;
+
+        db.run(sql, [gameId, stepOrder, fromStationId, toStationId, eventId, coinsAfterStep], function (err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(this.lastID);
+            }
+        });
+    });
+}
+
+// Get ranking of games
+export const getRanking = () => {
+    return new Promise((resolve, reject) => {
+        const sql = `SELECT u.id AS userId,
+            u.username AS username,
+            u.name AS name,
+            MAX(g.score) AS bestScore
+            FROM users u
+            JOIN games g ON u.id = g.userId
+            WHERE g.status = 'completed'
+            GROUP BY u.id
+            ORDER BY bestScore DESC
+        `;
+
+        db.all(sql, [], (err, rows) => {
+            if (err) {
+                reject(err);
+            } else {
+                const rankings = [];
+
+                for (const row of rows) {
+                    const user = new User(row.userId, row.username, row.name);
+
+                    rankings.push({
+                        user,
+                        bestScore: row.bestScore
+                    });
+                }
+
+                resolve(rankings);
+            }
+        });
+    });
+}
