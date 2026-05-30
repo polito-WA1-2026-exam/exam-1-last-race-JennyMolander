899b65e1f5ae651bdd3d0af0b4fc465ce3836659
Jenny Molander <jennymol@stud.ntnu.no>
2026-05-30 11:15:35 +0200

    Implement users-dao

---
+++ server/dao/users-dao.js
@@ -0,0 +1,47 @@
+import crypto from "crypto";
+import db from "../db.js";
+import { User } from "../entities.js";
+
+// Get user by username and password
+export const getUser = (username, password) => {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM users WHERE username = ?";
+
+        db.get(sql, [username], (err, row) => {
+            if (err) {
+                reject(err);
+            } else if (row === undefined) {
+                resolve(false);
+            } else {
+                const user = new User(row.id, row.username, row.name);
+
+                crypto.scrypt(password, row.salt, 32, function(err, hashedPassword) {
+                    if (err) reject(err);
+                    if (!crypto.timingSafeEqual(Buffer.from(row.hash, "hex"), hashedPassword))
+                        resolve(false);
+                    else
+                        resolve(user);
+                })
+            }
+        })
+    })
+}
+
+// Get user by id
+export const getUserById = (userId) => {
+    return new Promise((resolve, reject) => {
+        const sql = "SELECT * FROM users WHERE id = ?";
+
+        db.get(sql, [userId], (err, row) => {
+            if (err) {
+                reject(err);
+            } else if (row === undefined) {
+                resolve(false);
+            } else {
+                const user = new User(row.id, row.username, row.name);
+
+                resolve(user);
+            }
+        })
+    })
+}
