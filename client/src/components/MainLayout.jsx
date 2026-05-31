3ebe9d19758c0a47c3b6d7ec84ca28d063f86081
Jenny Molander <jennymol@stud.ntnu.no>
2026-05-31 10:10:55 +0200

    Initial frontend architecture: App.jsx, main.jsx, tailwind

---
+++ client/src/components/MainLayout.jsx
@@ -0,0 +1,11 @@
+import { Outlet } from "react-router";
+
+function MainLayout() {
+    return (
+        <div className="min-h-screen flex flex-col bg-gray-100">
+            <p>Test</p>
+        </div>
+    )
+}
+
+export default MainLayout;
