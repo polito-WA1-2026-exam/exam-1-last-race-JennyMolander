1c69adf50f60d22390a3d1af1214f5e4259a1b7a
Jenny Molander <jennymol@stud.ntnu.no>
2026-05-31 10:55:41 +0200

    Implemented user context

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,22 @@ import { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import MainLayout from './components/MainLayout';
+import UserContext from './contexts/UserContext';
 
 function App() {
 
-  return (
-    <Container>
-      <Routes>
-        <Route path='/' element={<MainLayout />}>
+  const [user, setUser] = useState(null);
 
-        </Route>
-      </Routes>
-    </Container>
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      <Container>
+        <Routes>
+          <Route path='/' element={<MainLayout />}>
+
+          </Route>
+        </Routes>
+      </Container>
+    </UserContext.Provider>
   )
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Navbar } from 'react-bootstrap';
 function Header() {
     return (
         <Navbar className="flex justify-between items-center w-full bg-gray-800 text-white p-6">
-            
+            <h1 className="text-2xl">Last Race</h1>
         </Navbar>
     )
 }
